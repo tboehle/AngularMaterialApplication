22c10397c7b742f120bcb3556f39a3b430209682
Thorben <thorboeh@gmx.de>
2019-02-08 23:06:45 +0100

    Add sample angular-material-table with only example data

---
+++ src/app/angular-material-table/angular-material-table.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, MatSort } from '@angular/material';
+import { AngularMaterialTableDataSource } from './angular-material-table-datasource';
+
+@Component({
+  selector: 'material-angular-material-table',
+  templateUrl: './angular-material-table.component.html',
+  styleUrls: ['./angular-material-table.component.css']
+})
+export class AngularMaterialTableComponent implements OnInit {
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  dataSource: AngularMaterialTableDataSource;
+
+  /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
+  displayedColumns = ['id', 'name'];
+
+  ngOnInit() {
+    this.dataSource = new AngularMaterialTableDataSource(this.paginator, this.sort);
+  }
+}
